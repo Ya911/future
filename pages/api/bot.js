aa0bfbb0086d54458f5ac7f3fb0c9010e0878187
Ya911 <yi1@oulook.sa>
2022-12-06 01:45:01 +0300

    Official Version

--- pages/api/bot.js
+++ pages/api/bot.js
@@ -12,7 +12,7 @@ try {
       case 'POST':  
       if(!body)return res.status(500).json({error_code: 500 , description : "البيانات غير صحيحة يرجى أدخلها مره أخرى"})
       // let BodyChead = pick(body,['api_Token',"dojop"])
-      let {error} = await cheackDataBOT(BodyChead)
+      let {error} = await cheackDataBOT(body)
       if(error)return res.status(401).json({message : error})
       return res.status(200).json({message:"تم الأتصال"})
       default: return res.status(500).json(`Not Alowed method ${method}`)
